83101e740acbed389c90459d5c02c99fab767205
人久 <112744845+Renjiu13@users.noreply.github.com>
2024-12-04 15:35:14 +0800

    更新 vuepress.config.ts

--- vuepress.config.ts
+++ vuepress.config.ts
@@ -1,6 +1,7 @@
-import { viteBundler } from '@vuepress/bundler-vite'
-import { defineUserConfig } from 'vuepress'
-import { FileList } from './src/node/index.js'
+// 导入必要的模块
+import { viteBundler } from '@vuepress/bundler-vite';
+import { defineUserConfig } from 'vuepress';
+import { FileList } from './src/node/index.js';
 import { githubReleasesFilesAnalysis } from "./src/node/analysis/githubReleasesFilesAnalysis/index.js";
 import { cloudflarePagesDownProxy } from "./src/node/proxy/cloudflarePagesDownProxy/index.js";
 import { fileUrlTreeAnalysis } from "./src/node/analysis/fileUrlTreeAnalysis/index.js";
@@ -11,26 +12,51 @@ import { giteeReleasesFilesAnalysis } from './src/node/analysis/giteeReleasesFil
 import { githubReposAnalysis } from './src/node/analysis/githubReposAnalysis/index.js';
 import { giteeReposAnalysis } from './src/node/analysis/giteeReposAnalysis/index.js';
 
-
 /**
  * 站点配置文件，没有注释的选项如果不知道有什么作用不建议修改，有注释的选项可以根据注释修改
- * */
+ */
 export default defineUserConfig({
+  // 使用 Vite 作为构建工具
   bundler: viteBundler(),
+  // 页面文件的匹配模式
   pagePatterns: [],
+  // 网站的主要语言
   lang: 'zh-CN',
+  // 静态资源文件夹路径
   public: `./public`,
+  // 网站标题，标题颜色可在 src/client/css/main.css 中修改
   title: 'FList',
+  // 网站的简介，有助于搜索引擎收录
   description: 'FList - 将 GitHub Releases 以类似网盘的形式展示在网页上，方便用户下载开源软件。 支持视频、音频、图片、PDF 等文件的在线预览。',
+  // 页面 <head> 标签内添加的额外标签。不要修改/logo.png，可以替换掉这个文件，删除logo.png会导致构建出错。
   head: [['link', { rel: 'icon', href: '/logo.png' }]],
+  // 页面预加载，所有其它页面所需的文件都会被预拉取。这对于小型站点来说是十分有帮助的，因为它会大大提升页面切换的速度。但是在你的网站有很多页面时不建议你这么做。
+  // 简单来说就是，如果你的文件不多就可以打开这个选项，可以大大提高页面切换的速度，如果文件非常多就不建议打开。建议超过100个文件就不要打开这个选项。
   shouldPrefetch: true,
+  // 主题配置 FileList 是 vuepress 的一个主题，文件展示的功能全部由这个主题提供。
   theme: FileList([
+    // 挂载路径为 /example
     {
       mountPath: "/example",
+      // 使用 fileUrlTreeAnalysis 分析器，将外部 URL 文件挂载到网站
       analysis: fileUrlTreeAnalysis({
+        // 将 /test.jpg 路径挂载到 https://attach.52pojie.cn/forum/202412/04/094704u3qux9uqqxuua1hm.png
         "/test.jpg": "https://attach.52pojie.cn/forum/202412/04/094704u3qux9uqqxuua1hm.png"
       }),
     }
+    // 挂载测试单结构
+    {
+      mountPath:"/",
+      analysis:fileUrlTreeAnalysis({
+        "/example/test.jpg":"https://example.com/test.jpg",
+        "/test1.jpg":"https://example.com/test1.jpg",
+        "/test/test2.jpg":"https://example.com/test2.jpg",
+        "/example/test3.jpg":"https://example.com/test3.jpg",
+        "/example/test/test4.jpg":"https://example.com/test4.jpg",
+        .....
+      }),
+    }
+    
     // ... 可以配置多个挂载路径和仓库，以此类推
   ])
-})
+});

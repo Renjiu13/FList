ffa50c601afb217cb14db45f437c45b7b308c930
人久 <112744845+Renjiu13@users.noreply.github.com>
2024-12-07 23:13:31 +0800

    Update vuepress.config.ts

--- vuepress.config.ts
+++ vuepress.config.ts
@@ -72,7 +72,8 @@ const fileSourceConfigs = {
   videoFiles: {
     mountPath: "/视频",
     analysis: fileUrlTreeAnalysis({
-      "/哥哥.mp4": "https://img.confused.us.kg/file/1731225510419_哥哥.mp4"
+      "/哥哥.mp4": "https://img.confused.us.kg/file/1731225510419_哥哥.mp4",
+      "/恶人家庭共操家教小萝莉.mp4": "https://highlight-video.cdn.bcebos.com/video/6s/7c95a6be-95bb-11ef-80ee-6c92bf81a74c.mp4"
     })
   }
 };
